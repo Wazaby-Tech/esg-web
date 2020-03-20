93550014de07e636111bce2cbc37cfc05058c13d
Mike Holloway <michael.a.holloway@outlook.com>
2020-03-19 22:23:09 -0700

    Embed youtube on home page

--- src/src/pages/index.js
+++ src/src/pages/index.js
@@ -1,11 +1,16 @@
 import React from "react"
 import Layout from "../components/layout"
 import AddressAutocomplete from "../components/addressAutocomplete"
+import Youtube from "react-youtube"
 
 // <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places"></script>
 
 export default () => (
     <Layout>
+        <Youtube
+            videoId="kTkxZ18yldc"
+            // https://www.youtube.com/watch?v=kTkxZ18yldc
+        />
         <AddressAutocomplete />
     </Layout>
 )
