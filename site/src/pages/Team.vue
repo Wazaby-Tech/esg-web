4b0980c85aaf501dea5955b899fa324d39a18235
Mike Holloway <40776983+mholloway24@users.noreply.github.com>
2020-06-05 17:35:51 -0700

    Migrate from GatsbyJS => Gridsome (#3)
    * start gridsome migration and add favicons
    
    * Migrate to gridsome

---
+++ site/src/pages/Team.vue
@@ -0,0 +1,14 @@
+<template>
+    <Layout>
+        <h1>Team</h1>
+        <h3>Leonardo Basaldua</h3>
+    </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Team'
+  }
+}
+</script>
--- src/src/pages/howitworks.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import Layout from "../components/layout"
-
-export default () =>
-(
-    <Layout>
-        <h4>How It Works</h4>
-    </Layout>
-)
